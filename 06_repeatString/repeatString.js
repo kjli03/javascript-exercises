3aee80c80d67f8e8d608bdafdee2c2c912d18035
kjli03 <katherinejli03@gmail.com>
2025-10-28 16:17:02 -0700

    Complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let answer = "";
+    if (count < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < count; i++) {
+        answer+=string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
