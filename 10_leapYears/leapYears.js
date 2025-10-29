c81d84dcfb233e6914811f84af575e7295c90309
kjli03 <katherinejli03@gmail.com>
2025-10-28 19:39:20 -0700

    Complete 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof(year) !== "number") {
+        return "ERROR";
+    }
+    if (year < 0) {
+        return "ERROR";
+    }
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+    else if (year % 4 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
