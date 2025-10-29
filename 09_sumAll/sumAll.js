b99a1c2b0e8a8f714ce86bde5ee48c7379a19a41
kjli03 <katherinejli03@gmail.com>
2025-10-28 19:32:50 -0700

    Complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    sum = 0;
+    if (start > end) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+    if (start !== Math.floor(start) || end !== Math.floor(end)) {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (typeof(start) !== "number" || typeof(end) !== "number") {
+        return "ERROR";
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
