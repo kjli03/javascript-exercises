b03d21da5c80a4897a7a117561a6a451a5c8c326
kjli03 <katherinejli03@gmail.com>
2025-10-28 19:46:20 -0700

    Complete 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const { camelCase } = require("case-anything");
+
+const convertToCelsius = function(fahr) {
+  fahr -= 32;
+  fahr *= 5 / 9;
+  fahr = Math.round(fahr * 10) / 10;
+  return fahr;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  cels *= 9 / 5;
+  cels += 32;
+  cels = Math.round(cels * 10) / 10;
+  return cels;
 };
 
 // Do not edit below this line
