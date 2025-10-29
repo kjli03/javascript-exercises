b711ce6872f9cf5027aa9a1a35d3ccd0c70dd4f2
kjli03 <katherinejli03@gmail.com>
2025-10-28 19:21:00 -0700

    Complete 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    ans = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            ans.push(array[i]);
+        }
+
+    }
+    return ans;
+
 };
 
 // Do not edit below this line
