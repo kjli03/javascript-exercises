4659cda9f8af70e23bb169e5d5195e3a26930fd7
kjli03 <katherinejli03@gmail.com>
2025-11-07 17:20:51 -0800

    Complete 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const factorial = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
+};
+
+const factorial = function(num) {
+  let factorial = 1;
+	for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
