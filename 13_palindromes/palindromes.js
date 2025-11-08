645bc4e9e7a74da7b9e5987821a97fa2d2a187a2
kjli03 <katherinejli03@gmail.com>
2025-11-07 17:35:48 -0800

    Complete 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\W/g, '').toLowerCase();
+    let palindrome = "";
+    for (let i = string.length; i >= 0; i--) {
+        palindrome += string.charAt(i);
+    }
+    console.log(string);
+    palindrome = palindrome.replace(/\W/g, '').toLowerCase();
+    if (palindrome == string) {
+        return true;
+    }
+    console.log(palindrome);
+    return false;
 };
+
+palindromes("test a 123, wow");
 
 // Do not edit below this line
 module.exports = palindromes;
