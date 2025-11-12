909d3532bc1e50e631eba696e30901ef9719af16
kjli03 <katherinejli03@gmail.com>
2025-11-12 12:21:08 -0800

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fib = [0, 1, 1, 2, 3, 5, 8];
+    for (let i = 6; i < num + 1; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
